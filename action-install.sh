8da4d101b9b3f996d2c3faeb094869496ccf6a07
sbxlmdsl <sbxlm@qq.com>
2023-10-22 06:52:42 +0800

    Update action-install.sh

--- action-install.sh
+++ action-install.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-rime_version=9.5.14
-rime_git_hash=ef3b6ea
+rime_version=9.5.15
+rime_git_hash=d0a846c
 
 rime_archive="rime-${rime_git_hash}-macOS.tar.bz2"
 rime_download_url="https://github.com/sbxlmdsl/librime/releases/download/${rime_version}/${rime_archive}"
